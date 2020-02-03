5abd4c2384c92bb8f0ce1d2bc96c3626b16b9214
Anouar ElGhazaoui <2080049@talnet.nl>
2020-02-03 12:04:30 +0100

    Done

---
+++ for_klas.php
@@ -0,0 +1,9 @@
+<?php
+
+$klas = array("Sjonnie de Wiel", "Herman Kaal", "Henk de Steen", "Inge Kerkhoven", "Gert Huiswijk");
+
+$getal = count($klas);
+
+for($i = 0; $i <= $getal; $i++ ){
+    echo $klas[$i] . PHP_EOL;
+}
